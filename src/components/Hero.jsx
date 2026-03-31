03f73bdf55a37f65da4a336ac2891de6c54854b5
devikakprakasan <devikaprakasan123@gmail.com>
2026-03-31 16:14:12 +0530

    feat: add Hero component with animated tech orbit and responsive layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import { FiGithub, FiLinkedin, FiMail, FiArrowRight } from 'react-icons/fi';
+import { FaReact, FaNodeJs, FaHtml5, FaCss3Alt, FaDatabase } from 'react-icons/fa';
+import { SiJavascript, SiTailwindcss } from 'react-icons/si';
 import ParticleBackground from './ParticleBackground';
 import { TypeAnimation } from 'react-type-animation';
 import './Hero.css';
@@ -55,9 +57,38 @@ const Hero = () => {
         </div>
         
         <div className="hero-visual hide-on-scroll delay-1">
-          <div className="floating-shape shape-1 glass"></div>
-          <div className="floating-shape shape-2"></div>
-          <div className="floating-shape shape-3 glass"></div>
+          <div className="tech-orbit">
+            <div className="center-icon glass">
+              <FaReact size={50} color="#61DAFB" />
+            </div>
+            
+            <div className="orbit-path path-1">
+              <div className="orbit-icon-wrapper" style={{ animationDelay: '0s' }}>
+                <SiJavascript size={24} color="#F7DF1E" className="orbit-icon" />
+              </div>
+              <div className="orbit-icon-wrapper" style={{ animationDelay: '-5s' }}>
+                <FaHtml5 size={24} color="#E34F26" className="orbit-icon" />
+              </div>
+            </div>
+
+            <div className="orbit-path path-2">
+              <div className="orbit-icon-wrapper" style={{ animationDelay: '-2s' }}>
+                <FaCss3Alt size={24} color="#1572B6" className="orbit-icon" />
+              </div>
+              <div className="orbit-icon-wrapper" style={{ animationDelay: '-7s' }}>
+                <SiTailwindcss size={24} color="#06B6D4" className="orbit-icon" />
+              </div>
+            </div>
+
+            <div className="orbit-path path-3">
+              <div className="orbit-icon-wrapper" style={{ animationDelay: '-4s' }}>
+                <FaNodeJs size={24} color="#339933" className="orbit-icon" />
+              </div>
+              <div className="orbit-icon-wrapper" style={{ animationDelay: '-9s' }}>
+                <FaDatabase size={24} color="#4DB33D" className="orbit-icon" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
